abbef1024adea91ec35b09211525e3855a397547
Igot <ipopovic850@gmail.com>
2024-04-11 18:06:07 +0200

    dev

--- src/model/test_azure_credentials.py
+++ src/model/test_azure_credentials.py
@@ -8,14 +8,14 @@ workspace = run.experiment.workspace
 
 try:
     # Access the registered dataset by its name
-    dataset = Dataset.get_by_name(workspace, name='microsoft_mlops_training_dev')
-    
-    # Assuming the dataset is a tabular dataset, convert it to a pandas DataFrame (or use the appropriate method if it's a different type of dataset)
+    dataset = Dataset.get_by_name(workspace, \
+                                  name='microsoft_mlops_training_dev')
+
     df = dataset.to_pandas_dataframe()
 
     # Log that the dataset was accessed successfully
     run.log('DataAccessStatus', 'Success')
-    run.log('DataFrameShape', str(df.shape))  # Example of logging additional information like DataFrame shape
+    run.log('DataFrameShape', str(df.shape))
 except Exception as e:
     # Log failure to access the dataset along with the error message
     run.log('DataAccessStatus', 'Failure')
